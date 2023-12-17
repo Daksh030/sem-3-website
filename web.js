a06b8e0f19fab2c05155bf2c6a182514c505b9b5
Daksh030 <153002336+Daksh030@users.noreply.github.com>
2023-12-17 18:00:38 +0530

    JS

---
+++ web.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $('.my').slideDown(1200);
+//   });
+
+  $(document).ready(function(){
+    $("#sec").click(function(){
+      $("#first").slideDown(1000);
+    });
+  });
